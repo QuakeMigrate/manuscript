2538327858f28d74b239639fc52bed5ba0a4b2ba
Conor Bacon <conor.bacon@gmail.com>
2025-07-08 13:50:03 -0400

    :wrench: move Fig S4 -> Fig S5 EOM

--- figures/supplementary/profiling/build_figureS5.py
+++ figures/supplementary/profiling/build_figureS5.py
@@ -1,5 +1,5 @@
 """
-This script builds Figure S4 of the manuscript:
+This script builds Figure S5 of the manuscript:
 
     Winder, T., Bacon, C.A., Smith, J.D., Hudson, T.S., Drew, J., and White, R.S.
     QuakeMigrate: a Python Package for Automatic Earthquake Detection and Location
@@ -44,4 +44,4 @@ ax.set_ylim(bottom=0)
 ax.set_xlabel("Time / s")
 ax.set_ylabel("Memory used / GiB")
 
-fig.savefig("FigureS4.png", dpi=400)
+fig.savefig("FigureS5.png", dpi=400)
